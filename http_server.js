bcd6a781ebccdf9aa4cff551a3ef8386bc7cebf4
Will Jensen <jensen.ww@gmail.com>
2014-10-09 21:30:13 -0600

    serial port read take 1

--- http_server.js
+++ http_server.js
@@ -1,13 +1,22 @@
 var http = require('http');
+var SerialPort = require('serialport').SerialPort;
+var serialport = new SerialPort('/dev/ttyACM0')
 
 http.createServer( function(req,res) {
+	serialport.on('open', function(){
+		console.log('serial port opened');
+		serialport.on('data',function(data){
+			console.log(data[0])
+			var currentTime = new Date();
+			console.log('Client called at '+currentTime);
 
-   var currentTime = new Date();
-   console.log('Client called at '+currentTime);
-
-   res.writeHead(200, {'Content-Type':'text/plain'});
-   res.write('The House is still here...\n');
-   res.write('Enjoy the Beach !\n');
-   res.end();
+   			res.writeHead(200, {'Content-Type':'text/plain'});
+   			res.write('The temperature is' + data[0]);
+   			//res.write('Enjoy the Beach !\n');
+   			res.end();
+		});
+	});
+   
+   
 
 }).listen('80');
