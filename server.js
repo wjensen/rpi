641f15cb6ece20f8e4cfe3b15f91994ebe7320ef
Will Jensen <jensen.ww@gmail.com>
2014-10-10 21:55:45 -0600

    serial port read take 15

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ SerialPort = require("serialport").SerialPort
 
 var socketServer;
 var serialPort;
-var portName = 'COM3'; //change this to your Arduino port
+var portName = '/dev/ttyACM0'; //change this to your Arduino port
 var sendData = "";
 
 // handle contains locations to browse to (vote and poll); pathnames.
