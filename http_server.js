2b60f2c8270d03feff374a00b439ae32fa6eb8db
Will Jensen <jensen.ww@gmail.com>
2014-10-10 20:59:20 -0600

    serial port read take 6

--- http_server.js
+++ http_server.js
@@ -2,7 +2,19 @@ console.log('starting up')
 var http = require('http');
 var SerialPort = require('serialport').SerialPort;
 var serialport = new SerialPort('/dev/ttyACM0', { baudRate: 9600} )
+serialport.on('open', function(){
+		console.log('serial port opened');
+		serialport.on('data',function(data){
+			console.log(data[0])
+			var currentTime = new Date();
+			console.log('Client called at '+currentTime);
 
+   			res.writeHead(200, {'Content-Type':'text/plain'});
+   			res.write('The temperature is' + data[0]);
+   			//res.write('Enjoy the Beach !\n');
+   			res.end();
+		});
+	});
 http.createServer( function(req,res) {
 	serialport.on('open', function(){
 		console.log('serial port opened');
